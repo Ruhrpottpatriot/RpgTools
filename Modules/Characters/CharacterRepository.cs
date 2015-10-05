0bff9612e7a67a9e8f29c86cbc7b826762cc1ef1
Ruhrpottpatriot <Ruhrpottpatriot@gmx.de>
2015-10-05 23:56:19 +0200

    Change CharacterRepository to use new async operations

--- Modules/Characters/CharacterRepository.cs
+++ Modules/Characters/CharacterRepository.cs
@@ -16,6 +16,8 @@ namespace RpgTools.Characters
     using System.Diagnostics.CodeAnalysis;
     using System.Globalization;
     using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
 
     using EntityFramework.Extensions;
 
@@ -36,16 +38,9 @@ namespace RpgTools.Characters
         private readonly IConverter<IDataContainer<Character>, CharacterItem> writeConverter;
 
         /// <summary>Initializes a new instance of the <see cref="CharacterRepository"/> class.</summary>
-        [SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "Only used for Entity Framework. This class should not be used in live code.")]
-        public CharacterRepository()
-            : this(new CharacterReadConverter(), new CharacterWriteConverter())
-        {
-        }
-
-        /// <summary>Initializes a new instance of the <see cref="CharacterRepository"/> class.</summary>
         /// <param name="characterReadConverter">The character data contract converter.</param>
         /// <param name="characterWriteConverter">The character Write Converter.</param>
-        internal CharacterRepository(IConverter<CharacterItem, Character> characterReadConverter, IConverter<Character, CharacterItem> characterWriteConverter)
+        public CharacterRepository(IConverter<CharacterItem, Character> characterReadConverter, IConverter<Character, CharacterItem> characterWriteConverter)
             : base("name=RpgTools")
         {
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<CharacterRepository, Configuration>(true));
@@ -56,65 +51,120 @@ namespace RpgTools.Characters
             this.writeConverter = new DataConverter<Character, CharacterItem>(characterWriteConverter);
         }
 
+        /// <summary>Initializes a new instance of the <see cref="CharacterRepository"/> class.</summary>
+        [SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "Only used for Entity Framework. This class should not be used in live code.")]
+        public CharacterRepository()
+            : this(new CharacterReadConverter(), new CharacterWriteConverter())
+        {
+        }
+
         /// <summary>Gets or sets the locale.</summary>
         public CultureInfo Culture { get; set; }
 
         /// <summary>Gets the internal character list.</summary>
-        internal DbSet<CharacterItem> Characters { get; private set; }
+        internal DbSet<CharacterItem> Characters { get; }
 
         /// <inheritdoc />
-        public Character Find(Guid identifier)
+        public Task<Character> FindAsync(Guid identifier)
         {
-            IDataContainer<CharacterItem> data = this.CreateContainer(
-                this.Characters.Include(c => c.Appearance).Include(c => c.Metadata).Single(c => c.Id == identifier),
-                this.Culture);
-
-            return this.readConverter.Convert(data);
+            return this.FindAsync(identifier, CancellationToken.None);
         }
 
         /// <inheritdoc />
-        public IDictionaryRange<Guid, Character> FindAll(ICollection<Guid> identifiers)
+        public async Task<Character> FindAsync(Guid identifier, CancellationToken cancellationToken)
         {
-            IDataContainer<ICollection<CharacterItem>> data = this.CreateContainer<ICollection<CharacterItem>>(
-                this.Characters.Include(c => c.Appearance).Include(c => c.Metadata).Where(c => identifiers.Any(i => i == c.Id)).ToList(),
-                this.Culture);
+            Task<IDataContainer<CharacterItem>> data = Task.Run(() => this.CreateContainer(this.Characters.Include(c => c.Appearance).Include(c => c.Metadata).Single(c => c.Id == identifier), this.Culture, DateTimeOffset.UtcNow), cancellationToken);
 
-            return this.bulkReadConverter.Convert(data);
+            return this.readConverter.Convert(await data);
         }
 
         /// <inheritdoc />
-        public IDictionaryRange<Guid, Character> FindAll()
+        public Task<IDictionaryRange<Guid, Character>> FindAllAsync()
         {
-            IDataContainer<ICollection<CharacterItem>> data = this.CreateContainer<ICollection<CharacterItem>>(
+            return this.FindAllAsync(CancellationToken.None);
+        }
+
+        /// <inheritdoc />
+        public async Task<IDictionaryRange<Guid, Character>> FindAllAsync(ICollection<Guid> identifiers, CancellationToken cancellationToken)
+        {
+            Task<IDataContainer<ICollection<CharacterItem>>> data = Task.Run(
+                () =>
+                this.CreateContainer<ICollection<CharacterItem>>(
+                    this.Characters
+                    .Include(c => c.Appearance)
+                    .Include(c => c.Metadata)
+                    .Where(c => identifiers
+                        .Any(i => i == c.Id))
+                    .ToList(),
+                    this.Culture,
+                    DateTimeOffset.UtcNow), cancellationToken);
+
+            return this.bulkReadConverter.Convert(await data);
+        }
+
+        /// <inheritdoc />
+        public Task<IDictionaryRange<Guid, Character>> FindAllAsync(ICollection<Guid> identifiers)
+        {
+            return this.FindAllAsync(identifiers, CancellationToken.None);
+        }
+
+        /// <inheritdoc />
+        public async Task<IDictionaryRange<Guid, Character>> FindAllAsync(CancellationToken cancellationToken)
+        {
+            Task<IDataContainer<ICollection<CharacterItem>>> data = Task.Run(
+                () => this.CreateContainer<ICollection<CharacterItem>>(
                this.Characters.Include(c => c.Appearance).Include(c => c.Metadata).ToList(),
-               this.Culture);
+               this.Culture,
+               DateTimeOffset.UtcNow), cancellationToken);
 
-            return this.bulkReadConverter.Convert(data);
+            return this.bulkReadConverter.Convert(await data);
+        }
+
+        /// <summary>Deletes an item in the repository.</summary>
+        /// <param name="identifier">The identifier that uniquely identifies an item in the repository.</param>
+        public Task DeleteAsync(Guid identifier)
+        {
+            throw new NotImplementedException();
         }
 
         /// <inheritdoc />
-        public void Delete(Guid data)
+        public async Task DeleteAsync(Guid data, CancellationToken cancellationToken)
         {
             this.Characters.Where(c => c.Id == data).Delete();
-            this.SaveChanges();
+
+            await this.SaveChangesAsync(cancellationToken);
         }
 
         /// <summary>Creates a new item in the repository.</summary>
         /// <param name="data">The data to write to the repository.</param>
-        public void Create(IDataContainer<Character> data)
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public Task CreateAsync(IDataContainer<Character> data)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public async Task CreateAsync(IDataContainer<Character> data, CancellationToken cancellationToken)
         {
             CharacterItem convertedData = this.writeConverter.Convert(data);
             this.Characters.Add(convertedData);
-            this.SaveChanges();
+            await this.SaveChangesAsync(cancellationToken);
         }
 
         /// <summary>Updates an item in the repository with the given data.</summary>
         /// <param name="data">The new data to store in the repository.</param>
-        public void Update(IDataContainer<Character> data)
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public Task UpdateAsync(IDataContainer<Character> data)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public async Task UpdateAsync(IDataContainer<Character> data, CancellationToken cancellationToken)
         {
             CharacterItem convertedData = this.writeConverter.Convert(data);
             this.Characters.AddOrUpdate(convertedData);
-            this.SaveChanges();
+            await this.SaveChangesAsync(cancellationToken);
         }
 
         /// <inheritdoc />
